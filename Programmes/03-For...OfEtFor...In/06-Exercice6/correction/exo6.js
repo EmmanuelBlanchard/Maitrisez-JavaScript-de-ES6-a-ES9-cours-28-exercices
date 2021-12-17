af002d16ddf90750fdd053205b57a94aac37f9ec
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-17 22:14:08 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-03-For...OfEtFor...In-E06-Correction-MoyenneD1TableauDeNotesEnParametre

---
+++ Programmes/03-For...OfEtFor...In/06-Exercice6/correction/exo6.js
@@ -0,0 +1,27 @@
+const notes = [5,10,15,20];
+
+function calculMoyenne(){
+    let resultat = 0;
+    for(let valeur of notes){
+        resultat += valeur;
+    }
+    return (resultat /= notes.length);
+}
+function calculMoyenne2(){
+    let resultat = 0;
+    for(let indice in notes){
+        resultat += notes[indice];
+    }
+    return (resultat /= notes.length);
+}
+console.log(`La moyenne est de ${calculMoyenne()}`);
+console.log(`La moyenne est de ${calculMoyenne2()}`);
+
+function calculMoyenne3(){
+    let resultat = 0;
+    for(let i = 0 ; i < notes.length ; i++){
+        resultat += notes[i];
+    }
+    return (resultat /= notes.length);
+}
+console.log(`La moyenne est de ${calculMoyenne3()}`);
