25146adb42dfee94b967c2093117dcfa6e64f061
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-17 21:57:38 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-03-For...OfEtFor...In-E06-Sujet-MoyenneD1TableauDeNotesEnParametre

---
+++ Programmes/03-For...OfEtFor...In/06-Exercice6/sujet/exo6.js
@@ -0,0 +1,15 @@
+/*
+
+Sujet : 
+
+    Créer une fonction qui permet de calculer la moyenne 
+    d'un tableau de notes passé en paramètre en utilisant les boucles :
+    
+        => For...of 
+        => For...in
+
+    Tester avec le tableau suivant : const notes = [5,10,15,20];
+
+    Réaliser l'affichage (en utilisant une Template String) : La moyenne est de 12.5
+
+*/
