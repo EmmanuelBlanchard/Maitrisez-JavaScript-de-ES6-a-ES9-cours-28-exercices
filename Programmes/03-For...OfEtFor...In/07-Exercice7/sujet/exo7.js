d044aff267950ca0ea6f9e52af4a1bdbc9724dad
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-17 22:25:34 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-03-For...OfEtFor...In-E07-Sujet-ProgrammePilesOuFaces

---
+++ Programmes/03-For...OfEtFor...In/07-Exercice7/sujet/exo7.js
@@ -0,0 +1,18 @@
+/*
+
+Sujet : 
+
+    Créer un programme qui va permettre de faire 10 fois << pile ou face >> 
+    et qui comptera le nombre de piles ou faces obtenus.
+
+    Afin d'utiliser les tableaux et les boucles for , suivez les étapes ci-dessous :
+
+    Étape 1 : créer le tableau <<randoms>> qui contiendra la liste de toutes les valeurs aléatoires générées (0 ou 1).
+
+    Étape 2 : parcourir le tableau <<randoms>> généré depuis l'étape 1 pour compter le nombre de piles ou faces
+
+    Étape 3 : faire l'affichage  suivant en utilisant les Template String : 
+
+    Le nombres de piles est de : 6, le nombre des faces est de : 4, le nombre de faces représente : 40%
+    
+*/
