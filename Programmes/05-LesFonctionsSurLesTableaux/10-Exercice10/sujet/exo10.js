1aff64d00a67d73ea714662b9c68f6051f021529
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-18 18:58:48 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-05-LesFonctionsSurLesTableaux-E10-Sujet(BoucleForOfEtFonctionEntries

---
+++ Programmes/05-LesFonctionsSurLesTableaux/10-Exercice10/sujet/exo10.js
@@ -0,0 +1,12 @@
+/* Sujet 
+
+    A partir du tableau de l'exercice 6, utiliser une boucle for...of et la fonction entries()
+
+    const notes = [5,10,15,20];
+
+    =>  
+        Note numéro 0 : 5
+        Note numéro 1 : 10
+        Note numéro 2 : 15
+        Note numéro 3 : 20
+*/
