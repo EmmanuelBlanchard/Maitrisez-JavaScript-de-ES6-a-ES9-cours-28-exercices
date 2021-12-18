cfcfcd0520a800b948cf60785f0881906dc2c0d5
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-18 20:56:10 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-05-LesFonctionsSurLesTableaux-E12a-Correction(programmeSupprimerPersonneDuTableauAPartirDeSonNom)

---
+++ Programmes/05-LesFonctionsSurLesTableaux/12-Exercice12/correction/exo12a.js
@@ -0,0 +1,28 @@
+let tab = [
+    { nom : "Tya",age : 25 },
+    { nom : "Toto",age : 22 },
+    { nom : "Milo",age : 32 },
+    { nom : "Mina",age : 32 },
+]
+
+// const indexMilo = tab.findIndex(function(perso) {
+//     return perso.nom === "Milo";
+// });
+
+// console.log(indexMilo);
+// console.log(tab[indexMilo]);
+
+// tab.splice(indexMilo,1);
+// console.log(tab);
+
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+
+function retourneIndexPersonne(nom) {
+    return tab.findIndex(function(perso) {
+        return perso.nom === nom;
+    });
+}
+
+tab.splice(retourneIndexPersonne("Toto"),1);
+console.log(tab);
