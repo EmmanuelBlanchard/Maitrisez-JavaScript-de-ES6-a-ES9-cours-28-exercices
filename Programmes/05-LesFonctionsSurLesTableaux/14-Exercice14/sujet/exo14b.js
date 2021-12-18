4bd3fbde629cc9d02bd861d1019c9823dcee70ac
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-18 23:02:16 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-05-LesFonctionsSurLesTableaux-E14b-Sujet(programmePalindromeOuNonHTML)

---
+++ Programmes/05-LesFonctionsSurLesTableaux/14-Exercice14/sujet/exo14b.js
@@ -0,0 +1,7 @@
+/* Sujet 
+
+    Utilisez la fonction palindrome dans une page HTML où l'utilisateur peut saisir un mot à vérifier.
+
+    Le programme affichera si celui-ci est un palindrome ou non.
+    
+*/
