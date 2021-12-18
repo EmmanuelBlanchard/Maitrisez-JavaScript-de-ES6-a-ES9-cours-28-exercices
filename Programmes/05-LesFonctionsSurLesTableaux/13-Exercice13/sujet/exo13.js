783f87ac49f989086b9ec84071b728c66b644c48
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-18 21:45:28 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-05-LesFonctionsSurLesTableaux-E13-Sujet(programmeTableauMotsSeTerminantEnOuMettreAuPluriel)

---
+++ Programmes/05-LesFonctionsSurLesTableaux/13-Exercice13/sujet/exo13.js
@@ -0,0 +1,15 @@
+/* Sujet 
+
+    A partir d'un tableau de mots se terminant en << ou >>, faites en sorte de mettre le pluriel correspondant
+    
+    const mots = ["clou","flou","bijou","genou","trou"];
+
+    Rappel : Les mots se terminant en << ou >> prennent un << s >> sauf exceptions
+
+    Exceptions : qui prennent un << x >> : bijou, caillou, genou, hibou, joujou, pou
+
+    Vous mettrez les mots au pluriel dans un nouveau tableau (proposez une solution utilisant les fonctions de tableau)
+    
+    Résultat : [ 'clous', 'flous', 'bijoux', 'genoux', 'trous'];
+
+*/
