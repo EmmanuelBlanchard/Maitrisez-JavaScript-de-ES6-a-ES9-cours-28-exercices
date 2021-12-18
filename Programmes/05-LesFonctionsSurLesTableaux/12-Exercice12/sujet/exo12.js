5c699e5b60b2cd0bd2131a43108c556504fd7dce
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-18 19:48:59 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-05-LesFonctionsSurLesTableaux-E12a-Sujet(programmeSupprimerPersonneDuTableauAPartirDeSonNom)

---
+++ Programmes/05-LesFonctionsSurLesTableaux/12-Exercice12/sujet/exo12.js
@@ -0,0 +1,21 @@
+/* Sujet 
+
+    Creer un programme permettant de supprimer une personne du tableau suivant,
+    à partir de son nom
+
+    Exemple : 
+
+        let tab = [
+            { nom : "Tya",age : 25 },
+            { nom : "Toto",age : 22 },
+            { nom : "Milo",age : 32 },
+            { nom : "Mina",age : 32 },
+        ]
+    
+    Exemple avec la suppresion de Milo: 
+
+    [ { nom : "Tya",age : 25 },
+      { nom : "Toto",age : 22 },
+      { nom : "Mina",age : 32 }, ]
+      
+*/
