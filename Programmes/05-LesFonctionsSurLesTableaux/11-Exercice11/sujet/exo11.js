c46b3347fb493cbd9448a7aaae125d333a0c4002
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-18 19:21:57 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-05-LesFonctionsSurLesTableaux-E11-Sujet

---
+++ Programmes/05-LesFonctionsSurLesTableaux/11-Exercice11/sujet/exo11.js
@@ -0,0 +1,11 @@
+/* Sujet 
+
+    Creer une fonction qui retourne un tableau ayant subi la transformation de chacune de ses cases pour contenir les précédentes valeurs au carré
+
+    Exemple : 
+
+    => Tableau en entrée : const notes = [5,10,15,20];
+
+   => Tableau transformé : [ 25, 100, 225, 400];
+
+*/
