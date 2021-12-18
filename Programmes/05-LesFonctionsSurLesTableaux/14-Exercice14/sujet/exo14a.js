9a8a076dbd7a1e6cb5dd2ef3629bdff3079dca3f
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-18 22:52:07 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-05-LesFonctionsSurLesTableaux-E14a-Sujet(programmepalindromeOuNon)

---
+++ Programmes/05-LesFonctionsSurLesTableaux/14-Exercice14/sujet/exo14a.js
@@ -0,0 +1,24 @@
+/* Sujet 
+
+    A partir d'un mot, créez un programme permettant d'indiquer si celui ci est un palindrome ou non.
+
+    Vous réaliserez la fonction estPalindrome(mot)
+    
+    Indication : un palindrome est un mot qui peut se lire indifféremment de gauche à droite
+    ou de droite à gauche.
+
+    Exemple : REVER => REVER
+    Contre exemple : BONJOUR => ROUJNOB
+    
+    Vous devez impérativement partir avec le mot à vérifier :  const mot = "rever";
+    const mots = ["clou","flou","bijou","genou","trou"];
+
+    Conseil : utiliser une fonction permettant de convertir un string en tableau
+
+    Voici l'affichage à réaliser : 
+
+    bonjour n'est pas un palindrome
+    
+    rever est un palindrome
+
+*/
