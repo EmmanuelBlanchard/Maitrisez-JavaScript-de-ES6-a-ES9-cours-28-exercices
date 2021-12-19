892d6134c3e6eaf545763523b22e47690822c813
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-19 21:22:31 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-07-OperateurSpreadEtRest-E17-Sujet(2FonctionsUtilisantOperateurRest)

---
+++ Programmes/07-OperateurSpreadEtRest/17-Exercice17/sujet/exo17.js
@@ -0,0 +1,21 @@
+/*
+
+    Sujet
+
+    Créez deux fonctions utilisant l'opérateur << Rest >> :
+    
+        -> La fonction << additionner >> permet d'additionner un nombre indéfini de nombre passés en paramètres
+   
+        -> La fonction << multiplier >> permet de multiplier un nombre indéfini de nombre passés en paramètres
+
+        Exemple d'utilisation : 
+
+        console.log("Le résultat est : " + additionner(5,10,15,20));
+        console.log("Le résultat est : " + multiplier(5,10,15,20));
+    
+        =>  Affichage 
+
+            Le resultat est : 50
+            Le resultat est : 15000
+        
+*/
