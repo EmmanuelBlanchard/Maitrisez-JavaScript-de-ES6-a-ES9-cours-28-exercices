c164cab6de49e4985f1b5d0380c798b8e5baf76f
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-19 21:40:55 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-07-OperateurSpreadEtRest-E17-Correction(2FonctionsUtilisantOperateurRest)

---
+++ Programmes/07-OperateurSpreadEtRest/17-Exercice17/correction/exo17.js
@@ -0,0 +1,17 @@
+function additionner(...nombres){
+    let resultat = 0;
+    for(let valeur of nombres){
+        resultat += valeur;
+    }
+    return resultat;
+}
+
+function multiplier(...nombres){
+    let resultat = 1;
+    for(let valeur of nombres){
+        resultat *= valeur;
+    }
+    return resultat;
+}
+console.log("Le résultat est : " + additionner(1,5,10,15,20,50,17,25));
+console.log("Le résultat est : " + multiplier(1,5,10,15,20,50,17,25));
