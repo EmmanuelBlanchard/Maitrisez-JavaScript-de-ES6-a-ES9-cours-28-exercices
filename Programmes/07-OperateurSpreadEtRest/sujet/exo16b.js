3ec178a3378e5c74de45c2e9c3cee1a45fd4d2d3
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-19 20:23:58 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-07-OperateurSpreadEtRest-E16b-Sujet(ObjetClasses)

---
+++ Programmes/07-OperateurSpreadEtRest/sujet/exo16b.js
@@ -0,0 +1,29 @@
+/*
+
+    Sujet
+
+    Ajoutez une fonction prenant en paramètres des personnages (en nombre illimité) et permettant de les afficher de cette maniere : 
+    
+    =>  Affichage 
+
+        *************************
+        
+        Nom : Milo
+        Age : 30
+        Classe : war
+        Liste des caractères :   
+        Force : 5
+        Agilite : 3
+        Intelligence : 2
+
+        *************************
+
+        Nom : Tya
+        Age : 22
+        Classe : archer
+        Liste des caractères :   
+        Force : 3
+        Agilite : 5
+        Intelligence : 3
+
+*/
