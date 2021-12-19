a133853e20c918f5240deb725bde805f119a5902
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-19 18:13:02 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-07-OperateurSpreadEtRest-E16a-Sujet(ObjetClasses)

---
+++ Programmes/07-OperateurSpreadEtRest/sujet/exo16.js
@@ -0,0 +1,31 @@
+/*
+
+    Sujet
+
+    Créez un objet << classes >> contenant trois objets
+
+    -> Guerrier { classe: 'war', force: 5, agilite: 3, intelligence: 2 }
+    -> Archer   { classe: 'archer', force: 3, agilite: 5, intelligence: 3 }
+    -> Mage     { classe: 'mage', force: 2, agilite: 3, intelligence: 5 }
+
+    Utilisez la structure pour créer 2 personnages :
+
+    -> Milo : c'est un guerrier qui a 30 ans
+    -> Tya : c'est une archère qui a 22 ans
+
+    =>  Affichage 
+
+        { nom: 'Milo',
+          age : 30,
+          classe: 'war', 
+          force: 5, 
+          agilite: 3, 
+          intelligence: 2 }
+        { nom: 'Tya',
+          age : 22,
+          classe: 'archer', 
+          force: 3, 
+          agilite: 5, 
+          intelligence: 3 }
+
+*/
