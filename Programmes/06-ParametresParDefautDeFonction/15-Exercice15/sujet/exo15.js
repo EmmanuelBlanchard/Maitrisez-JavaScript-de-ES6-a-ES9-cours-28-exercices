94d5631a6a8d0a9cefa6173157a593eb74bed1d0
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-19 16:21:37 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-06-ParametresParDefautDeFonction-E15-Sujet-afficherTableMultiplication

---
+++ Programmes/06-ParametresParDefautDeFonction/15-Exercice15/sujet/exo15.js
@@ -0,0 +1,48 @@
+/*
+
+    Sujet
+
+    Créez une fonction qui permet d'afficher les tables de multiplication d'un nombre.
+
+    Cette fonction pourra etre utilisée de plusieurs manières : 
+
+    1 paramètre : le nombre de la table de multiplication
+
+    2 paramètres : le nombre + le debut de la table
+
+    3 paramètres : le nombre + le debut de la table + la fin de la table
+
+    Si le debut et la fin sont inversés, la fonction doit tout de même fonctionner
+
+    afficherTableMultiplication(5);
+    console.log("*****************");
+    afficherTableMultiplication(5,7);
+    console.log("*****************");
+    afficherTableMultiplication(5,7,9);
+    console.log("*****************");
+    afficherTableMultiplication(5,9,7);
+
+    =>  Affichage 
+
+        1 * 5 = 5
+        2 * 5 = 10
+        3 * 5 = 15
+        4 * 5 = 20
+        5 * 5 = 25
+        6 * 5 = 30
+        7 * 5 = 35
+        8 * 5 = 40
+        9 * 5 = 45
+        10 * 5 = 50
+        *****************
+        7 * 5 = 35
+        8 * 5 = 40
+        9 * 5 = 45
+        *****************
+        7 * 5 = 35
+        8 * 5 = 40
+        *****************
+        7 * 5 = 35
+        8 * 5 = 40
+
+*/
