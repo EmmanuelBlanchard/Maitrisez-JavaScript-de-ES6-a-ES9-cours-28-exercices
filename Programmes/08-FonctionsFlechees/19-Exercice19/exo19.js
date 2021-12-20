0df49e7000531b34d42de256c4fd62a3d6e51cd0
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-20 15:25:33 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-08-FonctionsFlechees-E19-Sujet(fonctionsCalculMoyenne(tab)Ajout1Tab(tab)AdditionnerTab(tab1,tab2)

---
+++ Programmes/08-FonctionsFlechees/19-Exercice19/exo19.js
@@ -0,0 +1,30 @@
+/*
+    Sujet
+
+    A partir des deux tableaux suivants :
+
+    let notes1 = [5,10,15,20]; 
+    let notes2 = [2,4,6];
+
+    -> Créez les fonctions suivantes en utilisant la syntaxe des fonctions fléchées :
+
+        -> calculMoyenne(tab) : calcule la moyenne d'un tableau passé en paramètre et retourne le résultat
+
+        -> ajout1Tab(tab) : retourne un tableau contenant les valeurs, d'un tableau passé en paramètre, auquel on a ajouté 1
+
+        -> additionnerTab(tab1,tab2) : retourne un tableau contenant l'addition des valeurs à chaque indice du tableau. 
+        
+        Exemple :
+
+                    let notes1 = [5,10,15,20]; 
+                    let notes2 = [2,4,6];
+                    => [7,14,21,20];
+
+        Voici l'affichage attendu : 
+
+            La moyenne du tableau 1 est : 12.5
+            La moyenne du tableau 2 est : 4
+            Le tableau 1 majoré donne : 6,11,16,21
+            Le tableau 3 donne : 7,14,21,20
+
+*/
