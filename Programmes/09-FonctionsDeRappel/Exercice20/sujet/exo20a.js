8aeb6fa0adcbafa30e13c9852c3caaacbdddfeb2
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-20 21:23:05 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-09-FonctionsDeRappel-E20a-Sujet(FonctionsDeRappelsFonctionPermettantDeFormaterUneFonction)

---
+++ Programmes/09-FonctionsDeRappel/Exercice20/sujet/exo20a.js
@@ -0,0 +1,39 @@
+/*
+    Sujet 
+
+    En utilisant le concept des fonctions de rappels,créez une fonction qui permet de formatter le résultat d'une fonction.
+
+    -> Le formatage a réaliser est le suivant : 
+
+    **************
+
+    Le resultat est fonctionRappel
+    **************
+
+    -> Voici le résultat attendu pour 4 calculs différents : 
+
+        -> Addition de 5 par 3
+        -> Soustraction de 12 par 10
+        -> Multiplication de 10 par 11
+        -> Division de 5 par 3
+
+        **************
+    
+        Le resultat est 8
+        **************
+        **************
+    
+        Le resultat est 2
+        **************
+        **************
+    
+        Le resultat est 110
+        **************
+        **************
+    
+        Le resultat est 1.6666666666666667
+        **************
+
+        Il y a donc 4 fonctions de rappel a réaliser et une fonction permettant de formater
+
+*/
