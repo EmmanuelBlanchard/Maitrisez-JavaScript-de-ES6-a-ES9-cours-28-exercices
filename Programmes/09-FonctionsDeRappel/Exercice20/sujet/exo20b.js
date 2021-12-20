7a922db85099c98480d3f71a8019a94a7633d5c6
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-20 22:13:56 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-09-FonctionsDeRappel-E20b-Sujet(FonctionsDeRappelsFonctionPermettantDeFormaterUneFonction)

---
+++ Programmes/09-FonctionsDeRappel/Exercice20/sujet/exo20b.js
@@ -0,0 +1,32 @@
+/*
+    Sujet 
+
+    En apportant les modifications nécessaires, réutiliser ce qui a été programmé dans l' exercice pour créer la page suivante :
+    
+    Saisir le nombre 1 : 2
+    Saisir le nombre 2 : 3
+    multiplication
+    Valider
+    le résultat est 6
+
+    Saisir le nombre 1 : 2
+    Saisir le nombre 2 : 3
+    puissance
+    Valider
+    Calcul non pris en compte
+
+    -> A noter que note fonction de formatage ,renvoie un << div >> en couleur !
+
+    => <div id="resultat">
+        <div style="color:green;border:2px solid green">
+        le résultat est 6</div>
+       </div>
+
+    => <div id="resultat">
+        <div style="color:red;border:2px solid red">
+        Calcul non pris en compte</div>
+       </div>
+
+    -> Vous devez gérer les cas de calcul non pris en compte, exmple pour la << puissance >> présente dans la liste déroulante !
+
+*/
