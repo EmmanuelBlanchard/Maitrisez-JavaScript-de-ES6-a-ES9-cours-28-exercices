48e487ab5422ddf26faa3543084814062354cd0b
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-20 22:45:30 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-09-FonctionsDeRappel-E21-Sujet(FonctionsDeRappelsFonctionPileOuFace)

---
+++ Programmes/09-FonctionsDeRappel/21-Exercice21/sujet/exo21.js
@@ -0,0 +1,30 @@
+/* 
+    Sujet 
+
+    -> Créer la fonction << pile >> qui permet d'afficher un message en fonction
+    d'un nombre passé en paramètre de fonction : Vous avez obtenu un Pile 1
+
+    -> Créer la fonction << face >> qui permet d'afficher un message en fonction
+    d'un nombre passé en paramètre de fonction : Vous avez obtenu un Face 0
+
+    -> Créer la fonction << pileOuFace >> ayant deux paramètres (fonction de rappel)
+        -> un chiffre aléatoire est généré (0 ou 1)
+        -> Si le chiffre aléatoire est 0, alors la fonction de rappel numéro 1 est lance (avec le numéro alétoire généré)
+        -> Si le chiffre aléatoire est 1, alors la fonction de rappel numéro 2 est lance (avec le numéro alétoire généré)
+
+    -> Utiliser la fonction << pileOuFace >> avec les deux fonctions << pile >> et << face >>
+
+    Resultat de l'appel, Affichage :
+
+    pileOuFace(pile,face);
+
+    ********************
+
+    Vous avez obtenu un Face 0
+    //////////////////////////
+    ********************
+
+    Vous avez obtenu un Pile 1
+    //////////////////////////
+    
+*/
