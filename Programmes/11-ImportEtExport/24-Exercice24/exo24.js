5e28790909a9b24556c18215163063608206ac60
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-21 19:05:29 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-11-ImportEtExport-E24-Sujet

---
+++ Programmes/11-ImportEtExport/24-Exercice24/exo24.js
@@ -0,0 +1,18 @@
+/* 
+
+    Sujet 
+
+    -> Créez le module << calcul >> contenant :
+    
+        -> La fonction << moyenne >> calculant la moyenne d'un tableau passé en paramètre
+    
+        -> La fonction << somme >> calculant la somme d'un tableau passé en paramètre
+    
+    -> Utiliser le module << calcul >> pour calculer la moyenne des valeurs des tableaux suivants :
+
+                -> [5,10,15,20]
+                -> [2,4,6,8,10]
+
+    -> Le faire sur Node.JS puis sur une page web 
+    
+*/
