9355e3a0d7b9a6cf07334885f149cb72b96b8743
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-21 16:44:42 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-10-LesFonctionsAsynchrones-E22-Sujet

---
+++ Programmes/10-LesFonctionsAsynchrones/22-Exercice22/sujet/exo22.js
@@ -0,0 +1,35 @@
+/* 
+    
+    Sujet
+
+    -> Créez un programme qui permet de proposez un calcul mental à un utilisateur 
+    
+    et qui, après 6 secondes, donne le résultat
+
+    -> Le calcul utilisé est : 10 * 5 pour l'exemple.
+     
+    -> Toutes les deux secondes, le programme affiche un message
+    (voir le process décrit avec les impressions d'écran)
+
+    Affichage =>
+
+    Essayez de faire le calcul 10 * 5
+    (Après deux secondes =>)
+
+    Essayez de faire le calcul 10 * 5
+    Il vous reste 4 secondes
+
+    (Après deux secondes =>)
+
+    Essayez de faire le calcul 10 * 5
+    Il vous reste 4 secondes
+    Il vous reste 2 secondes
+
+    (Après deux secondes =>)
+    
+    Essayez de faire le calcul 10 * 5
+    Il vous reste 4 secondes
+    Il vous reste 2 secondes
+    Le résultat de 10 * 5 est : 50
+
+*/
