27cc78ede7545696b4c33632b98eec95404fa7bd
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-21 21:58:37 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-12-LesObjetsEnJavaScript-E25a-Sujet

---
+++ Programmes/12-LesObjetsEnJavaScript/25-Exercice25/sujet/exo25a.js
@@ -0,0 +1,23 @@
+/*
+    Sujet 
+
+    -> Créez l'objet calcul contenant :
+    
+        -> 2 propriétés : x et y
+
+        -> 3 fonctions :
+                - addition : permet d'additionner les deux propriétés
+                - multiplication : permet de multiplier les deux propriétés
+                - affichageCalcul : permet d'effectuer le calcul demandé et de l'afficher
+                                    en fonction du signe passé en paramètre : 
+                    
+                                    -> * , x , X pour la multiplication
+                                    -> + pour l'addtion
+            
+        -> Réaliser un test avec les valeurs 5 pour x et 3 pour y sur l'addtion et la multiplication, en utilisant la syntaxe suivante :
+
+            calcul.affichageCalcul("*"); => Le résultat du calcul 5 * 3 = 15
+
+            calcul.affichageCalcul("+"); => Le résultat du calcul 5 + 3 = 8
+
+*/
