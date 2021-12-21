f10d8b85e898f32d9bf5af8297d1f69b8fe1c0b1
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-21 17:24:51 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-10-LesFonctionsAsynchrones-E23a-Sujet-SimpleTimer

---
+++ Programmes/10-LesFonctionsAsynchrones/23-Exercice23/sujet/exo23a.js
@@ -0,0 +1,18 @@
+/* 
+
+    Sujet 
+
+    Programmez un simple << timer >> en utilisant les fonctions << async >> 
+    et le mot clef << await >>
+
+    Affichage : 
+
+        5
+        4
+        3
+        2
+        1
+        0
+        GO !!!
+
+*/
