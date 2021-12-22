47c608973f2bbab74405e0bbedff31e7bf5bf7ef
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-22 17:04:22 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-13-LesClassesEtLaPOO-E28b-Sujet

---
+++ Programmes/13-LesClassesEtLaPOO/28-Exercice28/sujet/exo28b.js
@@ -0,0 +1,16 @@
+/* 
+
+    Sujet 
+
+    -> Utiliser le fichier cercle.js en ne conservant que la classe << cercle >> et réaliser la page web suivante :
+    
+    Saisir le rayon du cercle : 5  Selecteur => Tout  , Bouton => Valider
+    Cercle de rayon 5 - Périmètre : 31.41592653589793 - Aire : 78.53981633974483
+    
+    Saisir le rayon du cercle : 5  Selecteur => Perimètre  , Bouton => Valider
+    31.41592653589793
+
+    Saisir le rayon du cercle : 5  Selecteur => Aire  , Bouton => Valider
+    78.53981633974483
+
+*/
