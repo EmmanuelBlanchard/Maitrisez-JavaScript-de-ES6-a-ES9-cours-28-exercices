a89a3eb9e7d62545765200c31c2328654005aea8
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-22 15:18:56 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-13-LesClassesEtLaPOO-E27-Sujet

---
+++ Programmes/13-LesClassesEtLaPOO/27-Exercice27/sujet/exo27.js
@@ -0,0 +1,35 @@
+/* 
+
+    Sujet 
+
+    -> Créez deux classes dans des fichiers séparés :
+
+        -> Auteur : 
+        
+            - Attributs : nom, age,
+            - Fonctions : toString => permettant de retourner l'affichage des attributs
+
+            Affichage : 
+
+            Nom : Milo - Age : 31
+
+        -> Livre :
+
+            - Attributs : titre, nombreDePages, auteur (qui contiendra des auteurs de la classe << Auteur >>)
+            - Fonctions : toString => permettant de retourner l'affichage des attributs
+
+            Affichage : 
+
+            Titre :Algorithmique selon H2PROG - NombreDePages : 200 - Auteur : Nom : Milo - 
+            Age : 31
+        
+        
+    -> Créez un fichier principal qui importe les deux classes et :
+
+        -> Créez deux auteurs : Milo (31 ans) Tya (24ans)
+        
+        -> Créez le livre << Algorithmique selon H2PROG >> de 200 pages écrit par Milo.
+        
+        -> Afficher le livre
+        
+*/
