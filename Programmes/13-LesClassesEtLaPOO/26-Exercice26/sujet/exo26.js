59a227d71f8f4810b8e51bb9da5924de17a4aa0a
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-22 14:21:20 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-13-LesClassesEtLaPOO-E26-Sujet

---
+++ Programmes/13-LesClassesEtLaPOO/26-Exercice26/sujet/exo26.js
@@ -0,0 +1,36 @@
+/* 
+
+    Sujet 
+
+    -> Créez les classes :
+
+        -> Voiture, contenant une marque, un modèle, un nombre de portes
+
+        -> Voiture5portes : qui étend la classe Voiture en definissant le nombre de portes à 5
+
+        -> Voiture3portes : qui étend la classe Voiture en definissant le nombre de portes à 3
+
+        -> Voiture5portesYotota : qui étend la classe Voiture5portes en definissant la marque << Yotota >>
+
+        -> Voiture5portesTroen : qui étend la classe Voiture5portes en definissant la marque << Troen >>
+
+        -> Voiture5portesYototaRyas :  qui étend la classe Voiture5portesYotota en definissant le modèle << Ryas >>
+
+    -> Créez les voitures suivantes , en utilisant les classes les plus adpatées :
+
+        marque : 'Yotota'
+        modele : 'Ryas'
+        nbPortes : 5
+        type : 'familiale'
+
+        marque : 'Troen'
+        modele : '5C'
+        nbPortes : 5
+        type : 'familiale'
+
+        marque : 'Troen'
+        modele : '4C'
+        nbPortes : 3
+        type : 'celibataire'
+
+*/
