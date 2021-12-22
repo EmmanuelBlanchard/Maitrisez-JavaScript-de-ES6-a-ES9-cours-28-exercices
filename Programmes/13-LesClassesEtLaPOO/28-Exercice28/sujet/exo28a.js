1370c316b89b4656a198a4b196bef279599061fc
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-12-22 16:13:35 +0100

    Maitrisez-JavaScript-de-ES6-a-ES9-cours-28-exercices-13-LesClassesEtLaPOO-E28-Sujet

---
+++ Programmes/13-LesClassesEtLaPOO/28-Exercice28/sujet/exo28a.js
@@ -0,0 +1,25 @@
+/* 
+
+    Sujet 
+
+    -> Créez la classe << Cercle >> contenant l'attribut << rayon >> et les fonctions :
+
+        -> Perimètre : permettant de retourner le perimètre du cercle
+
+        -> Aire : permettant de retourner l'aire du cercle
+
+        -> toString : permettant de retourner une chaine de caractères pour afficher toutes les valeurs
+
+    -> Faire une boucle permettant d'afficher les cercles de rayon 2 à 10 :
+
+        Cercle de rayon 2 - Périmètre : 12.566370614359172 - Aire : 12.566370614359172
+        Cercle de rayon 3 - Périmètre : 18.84955592153876 - Aire : 28.274333882308138
+        Cercle de rayon 4 - Périmètre : 25.132741228718345 - Aire : 50.26548245743669
+        Cercle de rayon 5 - Périmètre : 31.41592653589793 - Aire : 78.53981633974483
+        Cercle de rayon 6 - Périmètre : 37.69911184307752 - Aire : 113.09733552923255
+        Cercle de rayon 7 - Périmètre : 43.982297150257104 - Aire : 153.93804002589985
+        Cercle de rayon 8 - Périmètre : 50.26548245743669 - Aire : 201.06192982974676
+        Cercle de rayon 9 - Périmètre : 56.548667764616276 - Aire : 254.46900494077323
+        Cercle de rayon 10 - Périmètre : 62.83185307179586 - Aire : 314.1592653589793
+
+*/
